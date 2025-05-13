b669fcc6f0313e50a5cfa4d171aff739ef384c8e
Rin 🌸 <rin-z4@proton.me>
2025-05-13 10:35:03 +0200

    Add types.h

---
+++ src/types.h
@@ -0,0 +1,37 @@
+#ifndef __CWL_TYPES_H_Fjva3QhX
+#define __CWL_TYPES_H_Fjva3QhX
+
+/* size_t */
+#include <stddef.h>
+
+/* Machine byte (>= 8 bits) */
+typedef unsigned char  ubyte;
+typedef   signed char  sbyte;
+
+/* Types of at least N bit width, hence the "p" suffix */
+typedef unsigned char  u8p;
+typedef   signed char  s8p;
+
+typedef unsigned short u16p;
+typedef   signed short s16p;
+
+typedef unsigned long  u32p;
+typedef   signed long  s32p;
+
+/* Shorthands */
+typedef unsigned char uchar;
+typedef   signed char schar;
+
+typedef unsigned short ushort;
+typedef   signed short sshort;
+
+typedef unsigned int   uint;
+typedef   signed int   sint;
+
+typedef unsigned long  ulong;
+typedef   signed long  slong;
+
+/* Misc */
+typedef ubyte bool_t;
+
+#endif
